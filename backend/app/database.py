ec135e5e0518b909b30099ecab3831fb97525759
Sophie-Muchiri12 <muthonisophie12@gmail.com>
2026-02-21 15:01:55 +0300

    incoming main database.py change

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,27 +6,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-<<<<<<< HEAD
-ENV = os.getenv("APP_ENV", "local")  # defaults to "local" if not set
-
-if ENV == "production":
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    # Render sometimes provides "postgres://" but SQLAlchemy needs "postgresql://"
-    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-else:
-    DB_USER = os.getenv("DB_USER")
-    DB_PASS = os.getenv("DB_PASS")
-    DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_PORT = os.getenv("DB_PORT", "3306")
-    DB_NAME = os.getenv("DB_NAME")
-    DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-=======
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
->>>>>>> main
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
